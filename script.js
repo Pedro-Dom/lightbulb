e4032422212f0f5e83c71d3fb17c895ffc8220cb
Pedro-Dom <pedroaugusto.dom@outlook.com>
2022-03-15 10:11:26 -0300

    Projeto finalizado

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function changeLights(){
 
     let nomeLampada = document.getElementById("light").src
     let bulb = nomeLampada.substring(nomeLampada.indexOf('light-') + 6)
+    console.log(bulb)
 
     if (bulb == 'off.png') {
     document.getElementById("light").src="images/light-on.png";
